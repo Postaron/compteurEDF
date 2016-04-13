344d58ff3bc1fca3afd74a5e4cfdaf1c131f5a4b
Postaron <Postaron@users.noreply.github.com>
2016-04-13 13:45:53 +0200

    Added files via upload

--- main.c
+++ main.c
@@ -13,6 +13,37 @@
 #define BAUDRATE (((F_CPU / (BAUD * 16UL))) - 1)
 
 volatile char data;
+unsigned char flag;
+
+void uart_init(void);
+unsigned char uart_flagCheck(void);
+
+ISR(USART_RX_vect) { // interruption when start bit set to 0 (receive begin)
+	while(!(UCSR0A & (1 << RXC0))); //wait until RXC0 buffer is full
+	data = UDR0; // get the data
+}
+
+int main(void) {
+	uart_init();
+
+	for (;;) {
+		if(data != 0){ //if data is not empty
+			flag = uart_flagCheck(); // flag to know if there transmission problem
+			switch (flag){
+			case 1 : // Frame error
+				break;
+			case 2 : // Parity error
+				break;
+			case 3 : // Data Overrun (not possible but you know, info is like magic sometimes)
+				break;
+			default :
+				break;
+			}
+		}
+	}
+
+	return 0;
+}
 
 void uart_init(void) {
 	// set baudrate to 1200bit/second
@@ -25,17 +56,4 @@ void uart_init(void) {
 	UCSR0B |= (1 << RXCIE0); // Enable interruption when RX buffer is full
 	sei();
 	// Enable global interruption
-}
-
-ISR(USART_RX_vect) {
-
-}
-
-int main(void) {
-	uart_init();
-
-	for (;;) {
-	}
-
-	return 0;
 }
