5c897cb3c552fd45390e07ef3413a08435a131e9
Postaron <florent.denef@gmail.com>
2017-01-05 18:50:44 +0100

    modification, amélioration, optimisation, révision de l'algo aussi

--- main.c
+++ main.c
@@ -4,34 +4,22 @@
 #define F_CPU 16000000UL
 #define BAUD 1200
 #define BAUDRATE (((F_CPU / (BAUD * 16UL))) - 1)
-
-volatile char data;
-unsigned char flag;
+#define START 0x01 //not the real one
+#define END 0x02 //same reason
 
 void uart_init(void);
-unsigned char uart_flagCheck(void);
-
-ISR(USART_RX_vect) { // interruption when UDR0 is full
-	data = UDR0; // get the data
-}
+void uart_checkError(void);
+char uart_getChar(void);
+void decode(char data);
 
 int main(void) {
 	uart_init();
 
 	for (;;) {
-		if(data != 0){ //if data is not empty
-			flag = uart_flagCheck(); // flag to know if there transmission problem
-			switch (flag){
-			case 1 : // Frame error
-
-				break;
-			case 2 : // Parity error
-				break;
-			case 3 : // Data Overrun (not possible but you know, info is like magic sometimes)
-				break;
-			default :
-				break;
-			}
+		char data = uart_getChar();
+		if (data != 0) { //if data is not empty
+			uart_checkError(); // reboot µC
+			decode(data);
 		}
 	}
 
@@ -46,7 +34,22 @@ void uart_init(void) {
 	UCSR0C = 0b00100100; /*asynchronous mode enable, parity even, 7-bit data
 	 1 stop bit */
 	UCSR0B |= (1 << RXEN0); // Enable RX
-	UCSR0B |= (1 << RXCIE0); // Enable interruption when RX buffer is full
-	sei();
-	// Enable global interruption
+}
+
+//not finished yet : reboot µC (watchdog ?)
+void uart_flagCheck(void) {
+	switch (UCSR0A) {
+	case (1 << FE0):
+		break;
+	case (1 << DOR0):
+		break;
+	case (1 << UPE0):
+		break;
+	default:
+		break;
+	}
+}
+
+void decode(char data) {
+
 }
