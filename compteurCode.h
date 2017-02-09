b4eb9d679828f86124495bd036fbb57e53b164ad
Postaron <florent.denef@gmail.com>
2017-02-09 11:55:03 +0100

    file containing the different code of the serial communication

---
+++ compteurCode.h
@@ -0,0 +1,14 @@
+/*
+ * compteurCode.h
+ *
+ *  Created on: 9 févr. 2017
+ *      Author: postaron
+ */
+
+#ifndef COMPTEURCODE_H_
+#define COMPTEURCODE_H_
+
+#define ETIQUETTE 0x65 //just an example
+
+
+#endif /* COMPTEURCODE_H_ */
